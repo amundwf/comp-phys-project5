6ad847be1e521b9809f1647e016bf387ddd57074
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-12-02 13:56:54 +0100

    debugging

--- cpp_codes/utils.hpp
+++ cpp_codes/utils.hpp
@@ -23,7 +23,7 @@ void diffusion2D();
 
 arma::vec ThomasAlgorithm(int n, arma::vec u, double a, double b, bool verbose);
 
-int JacobiSolver(int N, double dx, double dt, arma::mat &A, arma::mat &q, double abstol);
+int JacobiSolver(int N, double dx, double dt, arma::mat &A, arma::mat &A_prev, double abstol);
 
 
 #endif
