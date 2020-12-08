1d9aaf064f7f4f5d8692ea7480c557b6d6810d7e
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-12-08 15:03:54 +0100

    Update 1D_diffusion_plotting.py

--- py_codes/1D_diffusion_plotting.py
+++ py_codes/1D_diffusion_plotting.py
@@ -17,6 +17,7 @@ directory = "../results/1D_diffusion/"
 fig, ax = plt.subplots(2,2, figsize=(9,9), dpi = 80)
 ax = ax.flatten()
 j=0
+
 for filename in os.listdir(directory):
     if filename.endswith(".csv"):
         
@@ -26,40 +27,36 @@ for filename in os.listdir(directory):
         print(data.shape)
        
         if re.search('Explicit', filename):
-            print("plotting explicit")
-            pcm = ax[j].pcolormesh(data)
-            ax[j].set_xlabel("N Points")
-            ax[j].set_ylabel("Time Points")
-            ax[j].set_yscale("linear")
-            ax[j].set_title("{}".format(filename))
-            
-            fig.colorbar(pcm, ax = ax[j])
-            j+=1
-        else:
+            name = "Explicit"
+        elif re.search("Implicit", filename):
+            name = "Implicit"
+         elif re.search("Crank", filename):
+            name = "Crank Nicolson"
                  
-            pcm = ax[j].pcolormesh(data, vmin=0.0, vmax=1.0)
-            ax[j].set_xlabel("N Points")
-            ax[j].set_ylabel("Time Points")
-            ax[j].set_title("{}".format(filename))
-            fig.colorbar(pcm, ax = ax[j])
-            j+=1
-            
+        pcm = ax[j].pcolormesh(data, vmin=0.0, vmax=1.0)
+        ax[j].set_xlabel("N Points")
+        ax[j].set_ylabel("Time Points")
+        ax[j].set_title("{}".format(name))
+        fig.colorbar(pcm, ax = ax[j])
+        j+=1
+
 '''
-elif re.search("analytical", filename):
-    print("plotting analytic")
-    N_x = data.shape[1]
-    xList = np.linspace(0,1,N_x)
-    u = np.zeros((data.shape))
-    
-    for i in range(0, data.shape[0]-1):
-        u[i,:] = data.iloc[i,:] + xList
-    
-    pcm = ax[j].pcolormesh(u, vmin=0.0, vmax=1.0)
-    ax[j].set_xlabel("N Points")
-    ax[j].set_ylabel("Time Points")
-    ax[j].set_title("Analytic {} + $x_i/L$".format(filename))
-    fig.colorbar(pcm, ax = ax[j])
-    j+=1
+fig, ax = plt.subplots(2,2, figsize=(9,9), dpi = 80)
+ax = ax.flatten()
+j=0
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"):
+        
+        data = np.loadtxt(directory + filename, delimiter=",")
+        data = pd.DataFrame(data)
+        print(filename)
+        print(data.shape)
+        
+        ax[j].plot(data.iloc[5000,:])
+        ax[j].set_xlabel("N Points")
+        ax[j].set_ylabel("$u(x,t)$")
+        ax[j].set_title("{}".format(filename))
+        
+        j+=1
+        
 '''
-       
-plt.show()
