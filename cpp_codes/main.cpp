b001dcf42dbd1ae13527c42c184c6931cf5dc8af
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-12-08 10:38:25 +0100

    added question for before or after enrichment

--- cpp_codes/main.cpp
+++ cpp_codes/main.cpp
@@ -6,12 +6,13 @@ using namespace std;
 using namespace arma;
 
 int main(){
+    /*
     int n_x=100; int N_sum=12000;
     //double tFinal=1e-6; double tStep=tFinal*0.1/5;
-    double tFinal=1.4e-7; double tStep=5e-10;
+    double tFinal=1.0; double tStep=5e-5;
     analytical_solution_1D(n_x, tFinal, tStep, N_sum);
 
-    /*
+    */
     int dim; string lithosphereOrNot;
     
     cout << "Please enter if you want to run in 1D or 2D diffusion equation (int)" << endl;
@@ -23,17 +24,26 @@ int main(){
     }
     
     if (dim == 2){
-        cout << "Do you want to run the function for the Lithosphere? [Y/N]" << endl;
+        cout << "Do you want to run the function for the Lithosphere [Y] or Unit Test [N]? [Y/N]" << endl;
         cin >> lithosphereOrNot;
 
         if (lithosphereOrNot == "y"){
-            diffusion2DLithosphere();
+            string BeforeEnrichmentOrNot;
+            cout << "Do you want to run before enrichment? [Y/N]" << endl;
+            cin >> BeforeEnrichmentOrNot;
+
+            if (BeforeEnrichmentOrNot == "y"){
+                diffusion2DBeforeEnrichment();
+            }
+            else{
+                diffusion2DAfterEnrichment();
+            }
         }
         else{
             diffusion2D();
         }
     }
-    */
+    
 
     return 0;
 }
