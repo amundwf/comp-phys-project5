eba8d9e40ab18bd1892fc6a9e7b676d2d4a66b2e
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-12-10 10:47:48 +0100

    debugging

--- py_codes/2D_diffusion_plotting.py
+++ py_codes/2D_diffusion_plotting.py
@@ -13,26 +13,29 @@ import os
 import re
 
 #directory = "../results/2D_diffusion_before_enrichment/"
-directory = "../results/2D_diffusion_after_enrichment/"
-#directory = "../results/2D_diffusion/"
+#directory = "../results/2D_diffusion_after_enrichment/"
+directory = "../results/2D_diffusion/"
 
+
+# PLease enter the x dimension (Npoints)from the file you want
+# to load. 
+x_dim = 10
+        
 for filename in os.listdir(directory):
     if filename.endswith(".txt"):
+        
         if re.search("Ana", filename):
             name = "Analytic"
         else:
             name = "Numerical"
+            
         fig, axes = plt.subplots(3,2, figsize=(15, 15), dpi = 80)
         axes = axes.flatten()
         cube = np.loadtxt(directory + filename)
         
-        # PLease enter the x dimension in the csv file.
-        x_dim = 10
         cube = cube.reshape(-1,x_dim,x_dim)
-        
         print("The cube shape is: ",cube.shape)
         
-       
         t_dim = cube.shape[0]
         step = int(t_dim / len(axes))
         
@@ -48,8 +51,9 @@ for filename in os.listdir(directory):
             #ax.set_yticks([])
             
             i+=step
-            cbar = fig.colorbar(pcm, ax = ax, format="%.2e")
+            cbar = fig.colorbar(pcm, ax = ax, format="%.1e")
             cbar.set_label("Temperature /Kelvin")
+            
         fig.suptitle("{} Solution to the two dimensional diffusion equation".format(name))
         
         plt.savefig(directory + "{}.png".format(name))
