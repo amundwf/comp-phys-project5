b386a887080f24ca8d69bab7565b742e974c9cc4
Amund Fredriksen <amund.fredriksen@outlook.com>
2020-12-12 23:15:27 +0100

    Copied changes in main.cpp from main branch to amund.

--- cpp_codes/main.cpp
+++ cpp_codes/main.cpp
@@ -6,10 +6,11 @@ using namespace std;
 using namespace arma;
 
 int main(){
+    /*
     run_5c();
 
-    /*
-    int dim; string lithosphereOrNot;
+    */
+    int dim; string lithosphereOrNot; bool enrichment; string EnrichmentOrNot;
     
     cout << "Please enter if you want to run in 1D or 2D diffusion equation (int)" << endl;
     cin >> dim;
@@ -20,17 +21,27 @@ int main(){
     }
     
     if (dim == 2){
-        cout << "Do you want to run the function for the Lithosphere? [Y/N]" << endl;
+        cout << "Do you want to run the Lithosphere problem [Y] or Unit Test [N]? [Y/N]" << endl;
         cin >> lithosphereOrNot;
 
         if (lithosphereOrNot == "y"){
-            diffusion2DLithosphere();
+            
+            cout << "Do you want to run after the mantle has been enriched? [Y/N]" << endl;
+            cin >> EnrichmentOrNot;
+
+            if (EnrichmentOrNot == "y"){
+                enrichment = true;
+            }
+            else{
+                enrichment = false;
+            }
+            lithosphere(enrichment);
         }
         else{
             diffusion2D();
         }
     }
-    */
+    
 
     return 0;
 }
